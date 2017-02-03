bfdfee58cd37af12724a4a3fbbb7643ab6dcbc13
Julius <j.tay@hotmail.de>
2017-02-03 20:32:20 +0100

    trying backprop

--- model.py
+++ model.py
@@ -1,8 +1,13 @@
 import numpy as np
 from sklearn.datasets import fetch_mldata
-from scipy import ndimage, optimize
-import matplotlib
-import time, PIL
+import matplotlib.pyplot as plt
+import time, pickle, warnings
+warnings.filterwarnings('error')
+try:
+    warnings.warn(Warning())
+except Warning:
+    print('Warning was raised as an exception!')
+np.seterr(all='raise')
 
 #fetch training data
 mnist = fetch_mldata('MNIST original')
@@ -26,7 +31,7 @@ def setupKernel(dim0, dim1, w1):
             if j in target:
                 w1.itemset(j * w1.shape[1] + i, weights[count])
                 count += 1
-            print("done.")
+    print("done.")
     return w1
 
 
@@ -36,23 +41,25 @@ class Model():
         self.inputLayerSize = 28*28
         self.outputLayerSize = 10
         self.hiddenLayerSize = 24*24
-        self.reg_lambda = 0.01  # regularization strength
-        self.epsilon = 0.01     # learning rate
+        self.reg_lambda = 0.005  # regularization strength
+        self.epsilon = 0.01    # learning rate
 
         #weights
-        self.w1 = np.zeros((self.inputLayerSize, self.hiddenLayerSize))
+        self.w1 = np.zeros((self.inputLayerSize, self.hiddenLayerSize)) / np.sqrt(self.inputLayerSize)
         self.W1 = setupKernel(5, 5, self.w1)
+        self.b1 = np.zeros((1, self.hiddenLayerSize))
 
         self.W2 = np.random.randn(self.hiddenLayerSize,
-                                  self.outputLayerSize)
+                                  self.outputLayerSize) / np.sqrt(self.hiddenLayerSize)
+        self.b2 = np.zeros((1, self.outputLayerSize))
 
     def forward(self, X):
         #todo: validate
         #propagate input through network
         self.z2 = np.dot(X, self.W1)
-        self.a2 = self.sigmoid(self.z2)
+        self.a2 = np.tanh(self.z2)
         self.z3 = np.dot(self.a2, self.W2)
-        _y = self.sigmoid(self.z3)
+        _y = np.tanh(self.z3)
         out = self.softmax(_y)
 
         return out
@@ -61,14 +68,17 @@ class Model():
         return 1/(1+np.exp(-z))
 
     def sigmoidPrime(self, z):
-        #derivative of sigmoid function
-        return np.exp(-z)/((1+np.exp(-z))**2)
+                #derivative of sigmoid function
+        return self.sigmoid(z)*(1-self.sigmoid(z))
+
+    def tanh_deriv(self, x):
+        return 1.0 - np.tanh(x) ** 2
 
     def softmax(self, y):
-        self.exp_scores = np.exp(y)
 
-        self.probs = self.exp_scores / np.sum(self.exp_scores, keepdims=True)
-        return self.probs
+        exp_scores = np.exp(y)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        return probs
 
     def costFunction(self, X, y):
         #todo: validate
@@ -78,41 +88,67 @@ class Model():
         corect_logprobs = -np.log(self.yHat[range(len(X)), y])
         data_loss = np.sum(corect_logprobs)
         J = data_loss * (1. / (len(X)))
+
         return J
 
     def costFunctionPrime(self, X, y):
-        #todo: solve issues with X.T = [0. , .... , 0.]
-        y = y.astype(int)
-        self.yHat = self.forward(X)
-        self.yHat[range(len(X)), y] -= 1
-        dW2 = (self.a2.T).dot(self.yHat)
-        delta2 = self.yHat.dot(self.W2.T) * (1 - np.power(self.a2, 2))
-        dW1 = np.dot(X.T, delta2)
 
-        return dW1, dW2
+        y = y.astype(int)
+        delta3 = self.forward(X)
+
+        delta3[range(len(X)), y] -= 1
+
+
+        #delta3 = np.multiply(delta3, self.tanh_deriv(self.z3))
+
+        dW2 = (self.a2.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+
+        delta2 = np.dot(delta3, self.W2.T) * self.tanh_deriv(self.z2)
+
+        dW1 = np.dot(X.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+
+
+        return dW1, dW2, db1, db2
 
     def fit(self, X, y, num):
+        errors=[]
+        iterations=[]
+        loss_previous = self.costFunction(X, y)
         for i in range(num):
 
-            # backpropagation
-            dW1, dW2 = self.costFunctionPrime(X, y)
 
+
+            if i % 10 == 0:
+                # print("loss at iteration %r is %r\ndw1 is %r, dw2 is %r" % (i, self.costFunction(X, y), dW1, dW2))
+                loss = self.costFunction(X, y)
+                print("loss at iteration %r has decreased by  %r" % (i, (loss_previous-loss)))
+                errors.append(loss)
+                iterations.append(i)
+                loss_previous = loss
+
+            # backpropagation
+            dW1, dW2, db1, db2 =  self.costFunctionPrime(X, y)
+            #print(dW1, dW2)
             dW1 += self.reg_lambda
             dW2 += self.reg_lambda
+            #print(dW1.shape, dW2.shape)
 
             # gradient descent parameter update
             self.W1 += -self.epsilon * dW1
+            self.b1 += -self.epsilon * db1
             self.W2 += -self.epsilon * dW2
-
-            if i % 500 == 0:
-                print("loss at iteration %r is %r\ndw1 is %r, dw2 is %r" % (i, self.costFunction(X, y), dW1, dW2))
-
+            self.b2 += -self.epsilon * db2
+        plt.plot(iterations, errors)
+        plt.ylabel("Error")
+        plt.show()
 
 
 
 model = Model()
-print(model.costFunction(X, y))
-model.fit(X_test, y_test, 5000)
+#print(model.costFunction(X, y))
+model.fit(X_test, y_test, 1000)
 
 
 
